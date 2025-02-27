0b0ca4034111e7b02307192c6dfe6255c4d29418
doham <doha.mekouar@uphf.fr>
2025-02-27 10:37:44 +0100

    miaou

---
+++ frontend/src/test/java/fr/miaou/Frontend/FrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.miaou.Frontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
