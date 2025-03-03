982b6966d83abb2a6053d9aed60a2c70c3c0599e
w136761 <camille.guillier@worldline.com>
2025-03-03 10:16:01 +0100

    modif page message

--- frontend/src/main/java/fr/miaou/frontend/controller/HomeController.java
+++ frontend/src/main/java/fr/miaou/frontend/controller/HomeController.java
@@ -1,8 +1,13 @@
 package fr.miaou.frontend.controller;
 
+import fr.miaou.frontend.model.Message;
+import fr.miaou.frontend.model.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.Objects;
 
 @Controller
 public class HomeController {
@@ -16,11 +21,22 @@ public class HomeController {
 
     @GetMapping("/message")
     public String messagePage(Model model) {
+        this.username = "Garfield";
         if (username == null) {
             return "redirect:/";
         }
         this.username = username;
         model.addAttribute("username", username);
+        ArrayList<User> contacts = new ArrayList<>();
+        contacts.add(new User("John Doe"));
+        contacts.add(new User("Gary Smith"));
+        contacts.add(new User("Mary Smith"));
+        contacts.add(new User("John Smith"));
+
+        ArrayList<Message> messages = new ArrayList<>();
+        messages.add(new Message());
+        model.addAttribute("contacts", contacts);
+        model.addAttribute("messages", messages);
         return "messagePage";
     }
 
